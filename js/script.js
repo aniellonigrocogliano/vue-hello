a1fb42994307eca36ac1e6b815cfd48177f2f33e
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-17 14:15:27 +0200

    add: messaggio di benvenuto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+    return {
+     message:"Ciao utente benvenuto nella mio primoscript scritto con l'ausilio di vue",
+    };
+  },
+
+
+}).mount("#app");
